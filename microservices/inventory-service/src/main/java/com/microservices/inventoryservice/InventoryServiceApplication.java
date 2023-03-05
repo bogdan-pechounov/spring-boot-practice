ee11a0fda7d413cc94bd4bb7d244df0ed1c05119
Bogdan <bogdan.pechounov@gmail.com>
2023-03-05 12:01:14 -0500

    discovery server

--- microservices/inventory-service/src/main/java/com/microservices/inventoryservice/InventoryServiceApplication.java
+++ microservices/inventory-service/src/main/java/com/microservices/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.microservices.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.microservices.inventoryservice.model.Inventory;
 import com.microservices.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
