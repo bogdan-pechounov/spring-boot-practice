574461e0dd525f74c7e6e59f2886e17621737b86
Bogdan <bogdan.pechounov@gmail.com>
2023-04-07 19:21:14 -0400

    init

---
+++ java/src/main/java/com/bogdan/Main.java
@@ -0,0 +1,12 @@
+package com.bogdan;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args){
+        int[] numbers = new int[5];
+        numbers[1] = 1;
+        System.out.println(Arrays.toString(numbers));
+    }
+}
