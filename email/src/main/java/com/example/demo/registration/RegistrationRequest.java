906140074840eb0551188905619dfac15e7236f4
Bogdan <bogdan.pechounov@gmail.com>
2023-04-08 13:24:50 -0400

    send email

---
+++ email/src/main/java/com/example/demo/registration/RegistrationRequest.java
@@ -0,0 +1,10 @@
+package com.example.demo.registration;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+
+public record RegistrationRequest(String firstName, String lastName, String email, String password) {
+}
---
+++ email/src/main/java/com/example/demo/registration/token/ConfirmationTokenService.java
@@ -0,0 +1,27 @@
+package com.example.demo.registration.token;
+
+import com.example.demo.appuser.AppUserService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class ConfirmationTokenService {
+    private final ConfirmationTokenRepository confirmationTokenRepository;
+
+    public void saveConfirmationToken(ConfirmationToken token){
+        confirmationTokenRepository.save(token);
+    }
+    public Optional<ConfirmationToken> getToken(String token) {
+        return confirmationTokenRepository.findByToken(token);
+    }
+
+    public int setConfirmedAt(String token) {
+        return confirmationTokenRepository.updateConfirmedAt(
+                token, LocalDateTime.now());
+    }
+}
